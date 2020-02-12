c91e194ce0effbd63e0f0fa5064c7e80867a54da
Blin113 <54438172+Blin113@users.noreply.github.com>
2020-02-12 22:17:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool quit = true;
+
+            Console.WriteLine("1. Play");
+            Console.WriteLine("2. Quit");
+            String Do = Console.ReadLine();
+
+            switch (Do)
+            {
+                case "1":
+                    quit = false;
+                    break;
+
+                case "2":
+                    Console.WriteLine("Quit? (Y/N)");
+                    string QT = Console.ReadLine().ToLower();
+                    if (QT == "y")
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                    else if (QT == "n")
+                    {
+                        Console.Clear();
+                        Main(null);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Command, press any key to contionue");
+                        Console.ReadKey();
+                        Console.Clear();
+                        Main(null);
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Command, press any key to contionue");
+                    Console.ReadKey();
+                    Main(null);
+                    break;
+            }
+
+            while (!quit)
+            {
+                List<Stats> statlist = new List<Stats>();
+
+                statlist.Add(new Stats(5, 5, 5, 5, 5));
+
+                String action = Console.ReadLine() + "";
+
+                switch (action)
+                {
+                    case "1":
+
+                        break;
+
+                    case "2":
+                        Console.Clear();
+                        for (int i = 0; i < statlist.Count; i++)
+                        {
+                            Console.WriteLine("\n_____________________________________________________________________________");
+                            Console.WriteLine("Str: " + statlist[i].Strength);
+                            Console.WriteLine("Dex: " + statlist[i].Dexterity);
+                            Console.WriteLine("Agi: " + statlist[i].Agility);
+                            Console.WriteLine("Int: " + statlist[i].Inteligence);
+                            Console.WriteLine("Luck: " + statlist[i].Luck);
+                        }
+                        Console.WriteLine("\n1. Add");
+                        Console.WriteLine("2. Return");
+
+
+
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Stats.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp6
+{
+    class Stats
+    {
+        private int strength;
+        private int dexterity;
+        private int agility;
+        private int inteligence;
+        private int luck;
+
+        public Stats(int Str, int Dex, int Agi, int Int, int Luc)
+        {
+            strength = Str;
+            dexterity = Dex;
+            agility = Agi;
+            inteligence = Int;
+            luck = Luc;
+        }
+
+        public int Strength
+        {
+            get { return strength; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Invalid input");
+                    strength = 0;
+                }
+                else
+                {
+                    strength = value;
+                }
+            }
+        }
+
+        public int Dexterity
+        {
+            get { return dexterity; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Invalid input");
+                    dexterity = 0;
+                }
+                else
+                {
+                    dexterity = value;
+                }
+            }
+        }
+
+        public int Agility
+        {
+            get { return agility; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Invalid input");
+                    agility = 0;
+                }
+                else
+                {
+                    agility = value;
+                }
+            }
+        }
+
+        public int Inteligence
+        {
+            get { return inteligence; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Invalid input");
+                    inteligence = 0;
+                }
+                else
+                {
+                    inteligence = value;
+                }
+            }
+        }
+
+        public int Luck
+        {
+            get { return luck; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Invalid input");
+                    luck = 0;
+                }
+                else
+                {
+                    luck = value;
+                }
+            }
+        }
+    }
+}
